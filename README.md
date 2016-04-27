7eb13dbf7e4e8ecf03aca02224f24f8fcb17b83d
Silvan Adrian <hallo@silvanadrian.ch>
2016-04-27 09:00:09 +0200

    add file watchers

---
+++ README.md
@@ -0,0 +1,22 @@
+# WED1 Miniprojekt
+
+## How to Set Up
+
+```
+npm install
+```
+(Installs Gulp and Browser-Sync)
+
+
+## Run local Dev server
+
+```
+gulp
+```
+
+## To Do
+
+- [x] Add File Watchers for html, css and js for livereload of assets
+- [ ] Change to SCSS for easier building
+- [ ] Add html-validator to Gulp -> W3C (automatic validation while running gulp)
+- [ ] Add css-validator W3C as Task to Gulp
--- gulpfile.js
+++ gulpfile.js
@@ -2,12 +2,16 @@ var gulp        = require('gulp');
 var browserSync = require('browser-sync').create();
 
 // Static server
-gulp.task('browser-sync', function() {
+gulp.task('serve', function() {
     browserSync.init({
         server: {
             baseDir: "./website"
         }
     });
+    gulp.watch("website/**/*.html").on('change', browserSync.reload);
+    gulp.watch("website/**/*.css").on('change', browserSync.reload);
+    gulp.watch("website/**/*.js").on('change', browserSync.reload);
 });
 
-gulp.task('default', ['browser-sync']);
+
+gulp.task('default', ['serve']);
