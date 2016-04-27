23e0531bcc5228ff665f272588517eebe1e89f45
Silvan Adrian <hallo@silvanadrian.ch>
2016-04-27 09:48:06 +0200

    Add W3C HTMl validator

--- README.md
+++ README.md
@@ -14,9 +14,14 @@ npm install
 gulp
 ```
 
+## Validate HTML (W3C)
+```
+gulp validate-html
+```
+
 ## To Do
 
 - [x] Add File Watchers for html, css and js for livereload of assets
-- [ ] Change to SCSS for easier building
-- [ ] Add html-validator to Gulp -> W3C (automatic validation while running gulp)
+- [ ] Change to SCSS
+- [x] Add html-validator to Gulp -> W3C (automatic validation while running gulp)
 - [ ] Add css-validator W3C as Task to Gulp
--- gulpfile.js
+++ gulpfile.js
@@ -1,17 +1,26 @@
 var gulp        = require('gulp');
 var browserSync = require('browser-sync').create();
+var w3cjs = require('gulp-w3cjs');
+
+gulp.task('validate-html', function () {
+	gulp.src('website/**/*.html')
+		.pipe(w3cjs())
+		.pipe(w3cjs.reporter());
+});
 
 // Static server
-gulp.task('serve', function() {
+gulp.task('serve',['validate-html'], function() {
     browserSync.init({
         server: {
             baseDir: "./website"
         }
     });
-    gulp.watch("website/**/*.html").on('change', browserSync.reload);
+    gulp.watch("website/**/*.html",['validate-html',browserSync.reload]);
     gulp.watch("website/**/*.css").on('change', browserSync.reload);
     gulp.watch("website/**/*.js").on('change', browserSync.reload);
 });
 
 
-gulp.task('default', ['serve']);
+
+
+gulp.task('default', ['validate-html','serve']);
