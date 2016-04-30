2db3a835b024431b87e2607b29e7b0a3d0127e0c
Silvan Adrian <hallo@silvanadrian.ch>
2016-04-30 16:15:18 +0200

    change w3cjs that html errors get put on console

--- gulpfile.js
+++ gulpfile.js
@@ -1,13 +1,10 @@
 var gulp        = require('gulp');
 var browserSync = require('browser-sync').create();
 var w3cjs = require('gulp-w3cjs');
-var notify = require("gulp-notify");
-var plumber = require('gulp-plumber');
 
 gulp.task('validate-html', function () {
 	gulp.src('website/**/*.html')
 		.pipe(w3cjs())
-		.pipe(plumber({errorHandler: notify.onError("Error: <%= error.message %>")}))
 });
 
 // Static server
