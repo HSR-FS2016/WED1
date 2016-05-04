a5f4f4191f5566685d502110735317e82e50b08d
Silvan Adrian <hallo@silvanadrian.ch>
2016-05-04 13:59:02 +0200

    change Template to Jade and change Gulp for serving Website from ./dist

--- gulpfile.js
+++ gulpfile.js
@@ -2,33 +2,48 @@ var gulp        = require('gulp');
 var browserSync = require('browser-sync').create();
 var w3cjs = require('gulp-w3cjs');
 var jade = require('gulp-jade');
+var watch = require('gulp-watch');
+var filesToMove = [
+        './website/assets/**/*.*',
+				'./website/calculator/**/*.*'
+    ];
+var clean = require('gulp-clean');
 
 gulp.task('validate-html', function () {
-	gulp.src('website/**/*.html')
+	gulp.src('dist/**/*.html')
 		.pipe(w3cjs())
 });
 
-// Static server
-gulp.task('serve',['validate-html'], function() {
-    browserSync.init({
-        server: {
-            baseDir: "./website"
-        }
-    });
-    gulp.watch("website/**/*.html",['validate-html',browserSync.reload]);
-    gulp.watch("website/**/*.css").on('change', browserSync.reload);
-    gulp.watch("website/**/*.js").on('change', browserSync.reload);
-});
-
 gulp.task('templates', function() {
-  var YOUR_LOCALS = {};
-
-  gulp.src('./website/index.jade')
-    .pipe(jade({
-      locals: YOUR_LOCALS
-    }))
+  return gulp.src('./website/index.jade')
+    .pipe(jade({}))
     .pipe(gulp.dest('./dist/'))
 });
 
+gulp.task('move-files', function() {
+	return gulp.src(filesToMove, { base: './website/' })
+  .pipe(gulp.dest('./dist'));
+});
+
+gulp.task('jade-watch', ['templates','reload']);
+gulp.task('css-watch', ['move-files','reload']);
+gulp.task('js-watch', ['move-files','reload']);
+
+gulp.task('reload', ['templates'], function() {
+    browserSync.reload();
+});
+
+gulp.task('serve',['templates'], function() {
+  gulp.src(filesToMove, { base: './website/' })
+  .pipe(gulp.dest('./dist'));
+	browserSync.init({
+			server: {
+					baseDir: "./dist"
+			}
+	});
+	gulp.watch('./website/**/*.jade', ['jade-watch']);
+	gulp.watch('./website/**/*.css',['css-watch']);
+  gulp.watch('./website/**/*.js',['js-watch']);
+});
 
 gulp.task('default', ['validate-html','serve']);
