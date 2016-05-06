d16ad91669e209539f1713e4d852c2fa610b49c8
fabianbinna <fbinna@hsr.ch>
2016-05-06 11:01:36 +0200

    calculator buttonHandlers

--- website/calculator/scripts/calculator.js
+++ website/calculator/scripts/calculator.js
@@ -1,15 +1,40 @@
 /**
  * core
  */
-
+var r1;
+var r2;
 
 
 
 /**
  * UI
  */
+
+function buttonNumClickHandler (event) {
+   document.getElementById('output').innerHTML = event.target.id.toString();
+}
+
+function buttonOpClickHandler (event) {
+   document.getElementById('output').innerHTML = event.target.id.toString();
+}
+
+function buttonComClickHandler (event) {
+   document.getElementById('output').innerHTML = event.target.id.toString();
+}
+
+function addClickHandlerToButton(handler, buttonClass) {
+   var buttons = document.getElementsByClassName(buttonClass);
+   var i;
+   for (i = 0; i < buttons.length; i++) {
+      buttons[i].onclick = handler;
+   }
+}
+
+
 window.addEventListener('load', function() {
-
-
+   
+   addClickHandlerToButton(buttonNumClickHandler, 'number');
+   addClickHandlerToButton(buttonOpClickHandler, 'operator');
+   addClickHandlerToButton(buttonComClickHandler, 'command');
 
 });
