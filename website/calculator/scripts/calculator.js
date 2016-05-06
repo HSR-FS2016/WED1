f024ca9664a3930d68393ac2d75ec0361d9b22c0
fabianbinna <fbinna@hsr.ch>
2016-05-06 17:14:49 +0200

    calculator finished

--- website/calculator/scripts/calculator.js
+++ website/calculator/scripts/calculator.js
@@ -1,47 +1,82 @@
 /**
  * core
  */
-var register2 = "";
-var register1 = "";
-var operation = "";
 
-function setOperation(_operation) {
-   operation = _operation;
-   register2 = register1;
-   register1 = "";
+function calculate(left, right, operator) {
+   var _left = parseInt(left);
+   var _right = parseInt(right);
+   if(operator === "+")
+      return _left + _right;
+   if(operator === "-")
+      return _left - _right;
+   if(operator === "/") {
+      if(_right == 0)
+         return "divide by zero error";
+      return _left / _right;
+   }
+   if(operator === "*")
+      return _left * _right;
 }
+
 
 
 /**
  * UI
  */
 
+var operand = "";
+var operator = "";
+
+var output;
+var input;
+
 function buttonNumClickHandler (event) {
-   $("#input").append(event.target.value);
-   register1 = $("#input").text(); //toNum
+   input.text(input.text() + event.target.value);
 }
 
 function buttonOpClickHandler (event) {
-   setOperation(event.target.value);
-   render();
-}
-
-function buttonComClickHandler (event) {
+   operator = event.target.value;
+   if(operand === "") {
+      operand = input.text();
+      input.text("");
+   }
+   output.text(operand + " " + operator);
    
 }
 
-function render() {
-   $("#output").text(register2 + " " + operation);
-   $("#input").text(register1);
+function buttonEqualClickHandler (event) {
+   if(operand === "" || operand === "" || input.text() === "") {
+      output.text("error");
+      return;
+   }
+
+   var result = calculate(operand, input.text(), operator);
+
+   operand = result;
+   operator = "";
+   input.text("");
+   output.text(operand + " " + operator);
+}
+
+function buttonClearClickHandler (event) {
+   input.text("");
+   output.text("");
+   operand = "";
+   operator = "";
 }
 
 $(document).on("ready", function() {
    $("#output").text("Welcome");
    $(".number").on("click", buttonNumClickHandler);
    $(".operator").on("click", buttonOpClickHandler);
-   $(".command").on("click", buttonComClickHandler);
-   $("button").one("click", function() {
+   
+   $(".equal").on("click", buttonEqualClickHandler);
+   $(".clear").on("click", buttonClearClickHandler);
+   $("body").one("click", function() {
       $("#output").text("");
    });
+
+   output = $("#output");
+   input = $("#input");
 });
 
